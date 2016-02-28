cfa68548405216ad245fdb26cbc89db2d26a72fa
Santiago Lovera <santiago.lovera@gmail.com>
2016-02-28 10:04:02 -0300

    inicio novo modulo

---
+++ src/main/java/lovera/imagem/App.java
@@ -0,0 +1,13 @@
+package lovera.imagem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
