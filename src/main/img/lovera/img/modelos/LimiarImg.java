bab191ea24700fe05385ea92cefcb39d6467e8e4
Santiago Lovera <santiago.lovera@gmail.com>
2016-03-06 12:22:48 -0300

    Implementando limiarizacao

--- src/main/img/lovera/img/modelos/LimiarImg.java
+++ src/main/img/lovera/img/modelos/LimiarImg.java
@@ -1,5 +1,26 @@
 package lovera.img.modelos;
 
-public final class LimiarImg {
+import lovera.img.comum.Regras;
+import lovera.img.modelos_img.CinzaImg;
 
+public final class LimiarImg {
+	
+	private int[] arrayBinario;
+	private int limiar;
+	
+	public LimiarImg(CinzaImg cinza) {
+		Regras.validarBufferedImgCinza(cinza);
+		
+		encontrarLimiar();
+		
+	}
+	
+	private void encontrarLimiar(){
+		//TO DO
+		this.limiar = 254;//VALOR CHEGO DE FORMA MAGICA AINDA!!!!!!!!!
+	}
+	
+	public int[] getArrayBinarizado(){
+		return this.arrayBinario;
+	}
 }
