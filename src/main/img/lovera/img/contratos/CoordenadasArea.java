92c6c19b73ff9a7c8a632e99cb2bfa77bd98e9c8
Santiago Lovera <santiago.lovera@gmail.com>
2016-03-21 17:49:06 -0300

    Documentacao

--- src/main/img/lovera/img/contratos/CoordenadasArea.java
+++ src/main/img/lovera/img/contratos/CoordenadasArea.java
@@ -3,6 +3,12 @@ package lovera.img.contratos;
 import java.awt.Rectangle;
 import java.util.List;
 
+/**
+ * Interface que extende de Coordenadas.</br>
+ * Utilizada pelas classes que lidam com listas de Retangulos "areas" de uma imagem.
+ * @author Lovera
+ * @since 21/03/2016
+ */
 public interface CoordenadasArea extends Coordenadas{
 	
 	List<Rectangle> getAreas();
