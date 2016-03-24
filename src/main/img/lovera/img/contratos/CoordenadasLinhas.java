f4e2d560bde30676efe9ab54060b87eb0432bead
Santiago Lovera <santiago.lovera@gmail.com>
2016-03-24 17:55:30 -0300

    Documentacao

--- src/main/img/lovera/img/contratos/CoordenadasLinhas.java
+++ src/main/img/lovera/img/contratos/CoordenadasLinhas.java
@@ -3,6 +3,11 @@ package lovera.img.contratos;
 import java.awt.geom.Line2D;
 import java.util.List;
 
+/**
+ * Interface para modelos que produzem Coordenadas em linhas para uma imagem. 
+ * @author Lovera
+ * @since 24/03/2016
+ */
 public interface CoordenadasLinhas {
 
 	List<Line2D> getLinhas();
