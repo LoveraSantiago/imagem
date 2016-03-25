e5a4bd103251a6f861e679575abceb4c86275132
Santiago Lovera <santiago.lovera@gmail.com>
2016-03-25 05:01:21 -0300

    documentando

--- src/main/img/lovera/img/contratos/CoordenadasPonto.java
+++ src/main/img/lovera/img/contratos/CoordenadasPonto.java
@@ -3,6 +3,12 @@ package lovera.img.contratos;
 import java.awt.Point;
 import java.util.List;
 
+/**
+ * Interface que extende Coordenadas.</br>
+ * Utilizado pelas classes que lidam com listas de Pontos.
+ * @author Lovera
+ * @since 25/03/2016
+ */
 public interface CoordenadasPonto extends Coordenadas{
 
 	List<Point> getCoordenadas(); 
