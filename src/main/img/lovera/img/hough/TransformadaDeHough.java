65d2854e56b5d121557120f121946e9ef8cd5bcf
Santiago Lovera <santiago.lovera@gmail.com>
2016-03-27 08:34:20 -0300

    Corrigido bug estouro de indices

--- src/main/img/lovera/img/hough/TransformadaDeHough.java
+++ src/main/img/lovera/img/hough/TransformadaDeHough.java
@@ -4,7 +4,6 @@ import java.awt.Point;
 import java.awt.geom.Line2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
-import java.util.Arrays;
 
 import lovera.comuns.recursos.Regras;
 import lovera.img.comum.Pixel;
@@ -21,9 +20,7 @@ class TransformadaDeHough implements Executor{
 	private int eixoX;
 	
 	private BufferedImage img;
-	
-	private Point ponto;
-	
+
 	private Line2D linhaHough;
 	
 	static{
@@ -47,10 +44,9 @@ class TransformadaDeHough implements Executor{
 		Regras.validarPontoDentroDaImg(imgRecortada, ponto, this.getClass());
 		
 		this.img = imgRecortada;
-		this.ponto = ponto;
 		
-		this.eixoX = Math.max(this.img.getWidth(), this.img.getHeight());
-		int alturaMatriz = 2 * eixoX;
+		int alturaMatriz = 4 * Math.max(this.img.getWidth(), this.img.getHeight());		
+		this.eixoX = alturaMatriz / 2;
 		this.matrizVotos = new int[GRAUS + 1][alturaMatriz];
 	}
 
@@ -61,7 +57,6 @@ class TransformadaDeHough implements Executor{
 		return this;
 	}
 	
-	int contadorA = 0;
 	private void transformada(){
 		
 		Raster raster = this.img.getRaster();
@@ -71,9 +66,6 @@ class TransformadaDeHough implements Executor{
 				
 				int sample = raster.getSample(j, i, 0);
 				if(sample == Pixel.PREENCHIDO.getValor()){
-					
-//					DebugImgModelo.debugarImg(img, "debug", true);
-//					System.out.println("Chamado dentro do if " + contadorA++ + " vezes.");
 					
 					int[] polaresDoPonto = calcularPolarDoPonto(j, i);
 					inputarVotos(polaresDoPonto);
@@ -85,7 +77,6 @@ class TransformadaDeHough implements Executor{
 		setLinhaHough(linha);
 		
 		this.img         = null;
-		this.ponto       = null;
 		this.matrizVotos = null;
 	}
 	
@@ -98,24 +89,10 @@ class TransformadaDeHough implements Executor{
 		return polar;
 	}
 	
-	int contador = 0;
-	int exception = 0;
 	private void inputarVotos(int[] polaresDoPonto){
-			System.out.println("Chamado inputar votos " + contador++ + " vezes.");
 			for(int i = 1; i <= GRAUS; i++){
-				try{ 
-				if(contador == 108 && i == 66){
-					System.out.println("Novo break");
-				}
-//				System.out.println("Imputando votos " + polaresDoPonto[i]);
 				int ajustadoPEixoX = ajustarParaEixoX(polaresDoPonto[i]);
 				this.matrizVotos[i][ajustadoPEixoX]++;
-				}
-				catch(IndexOutOfBoundsException e){
-					System.out.println("valor de estouro " + polaresDoPonto[i]);
-					System.out.println(Arrays.toString(polaresDoPonto));
-//					System.out.println("break aqui quebrado " + exception++);
-				}
 			}		
 	}
 	
@@ -123,8 +100,6 @@ class TransformadaDeHough implements Executor{
 		return valorY + this.eixoX;
 	}
 	
-	int contadorB = 0;
-	int contadorC = 0;
 	private Point encontrarCoordenadaPolarMaisVotada(){
 		int vetorMVot = 0;
 		int anguloMVot = 0;
@@ -132,11 +107,8 @@ class TransformadaDeHough implements Executor{
 		int maisVotos = Integer.MIN_VALUE;
 		
 		for(int i = 1; i <= GRAUS; i++){
-//			System.err.println("matrizVotos graus " + contadorB++);
-			contadorC = 0;
 			for(int j = i; j < this.matrizVotos[i].length; j++){
 				
-//				System.out.println("matrizVotos vetores " + contadorC++);
 				int voto = this.matrizVotos[i][j];
 				if(voto > maisVotos){
 					maisVotos = voto;
