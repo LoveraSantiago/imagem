f5c3cfe3ca9183b038c67baae32bc9bab3691cab
Santiago Lovera <santiago.lovera@gmail.com>
2016-03-30 05:41:49 -0300

    Documentacao

--- src/main/img/lovera/img/graos/Area.java
+++ src/main/img/lovera/img/graos/Area.java
@@ -2,6 +2,12 @@ package lovera.img.graos;
 
 import java.awt.Rectangle;
 
+/**
+ * Objeto abstrato que serve como base para diversos momentos pela qual o 
+ * objeto Rectangle passa.  
+ * @author Lovera
+ * @since 30/03/2016
+ */
 abstract class Area {
 	
 	protected final Rectangle area;	
