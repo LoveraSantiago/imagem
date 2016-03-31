6d460cbf9bf78f750e35d8ea99520f97c59f6de9
Santiago Lovera <santiago.lovera@gmail.com>
2016-03-31 05:38:41 -0300

    Documentacao

--- src/main/img/lovera/img/graos/BlocoComLinha.java
+++ src/main/img/lovera/img/graos/BlocoComLinha.java
@@ -3,6 +3,12 @@ package lovera.img.graos;
 import java.awt.Rectangle;
 import java.awt.geom.Line2D;
 
+/**
+ * Objeto que armazena informações sobre os blocos que estao em volta dos CCs 'componentes conectados'. </br>
+ * Armazena para cada bloco um objeto line. 
+ * @author Lovera
+ * @since 31/03/2016
+ */
 public class BlocoComLinha extends Area{
 	
 	private final Line2D linha;
