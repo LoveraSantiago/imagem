f68ff4c3f8fa7733743df2aac1730dac47440da8
Santiago Lovera <santiago.lovera@gmail.com>
2016-04-01 18:51:03 -0300

    documentando

--- src/main/img/lovera/img/graos/BlocoComPonto.java
+++ src/main/img/lovera/img/graos/BlocoComPonto.java
@@ -3,6 +3,12 @@ package lovera.img.graos;
 import java.awt.Point;
 import java.awt.Rectangle;
 
+/**
+ * Objeto que armazena informações sobre os blocos que estao em volta dos CCs 'componentes conectados'. </br>
+ * Armazena para cada bloco um ponto, (geralmente o centro de gravidade)
+ * @author Lovera
+ * @since 01/04/2016
+ */
 public class BlocoComPonto extends Area{
 	
 	private final Point ponto;	
