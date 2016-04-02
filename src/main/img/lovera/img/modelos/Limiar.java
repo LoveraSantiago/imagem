7eb233a4cba6a9e4fd10e32a47415a54ed9843f8
Santiago Lovera <santiago.lovera@gmail.com>
2016-04-02 09:14:08 -0300

    Documentando

--- src/main/img/lovera/img/modelos/Limiar.java
+++ src/main/img/lovera/img/modelos/Limiar.java
@@ -4,6 +4,15 @@ import lovera.comuns.recursos.Regras;
 import lovera.img.comum.Pixel;
 import lovera.img.modelos.img.CinzaImg;
 
+/**
+ * Calcula um valor de limiar para binarizar a imagem.</br>
+ * <strong>Obs:</strong></br>
+ * -Ate o momento não faz nada alem de passar um valor 1 como limiar.
+ * -A aplicação teria de ser usada em outros exemplos para ver se responde de forma uniforme, do
+ * contrário uma implementação seria necessária 
+ * @author Lovera
+ * @since 02/04/2016
+ */
 public final class Limiar {
 	
 	private int[] arrayBinario;
